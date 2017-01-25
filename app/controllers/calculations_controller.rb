5b80161849bda8362bf7ff01f2694cd1f11c9dde
laurenhp <laurenpetersen@uchicago.edu>
2017-01-24 22:06:00 -0600

    omnicalc_final

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -5,19 +5,21 @@ class CalculationsController < ApplicationController
     @special_word = params[:user_word]
 
     # ================================================================================
-    # Your code goes below.
-    # The text the user input is in the string @text.
-    # The special word the user input is in the string @special_word.
+    @characters_no_spaces = @text.gsub(" ", "")
+    @text_into_array = @text.split
+    @downcased_input = @special_word.downcase
     # ================================================================================
 
 
-    @character_count_with_spaces = "Replace this string with your answer."
+    @character_count_with_spaces = @text.length
 
-    @character_count_without_spaces = "Replace this string with your answer."
+    @character_count_without_spaces = @text.delete(' ').delete("\r\n").length
 
-    @word_count = "Replace this string with your answer."
+    @word_count = @text.split.count
 
-    @occurrences = "Replace this string with your answer."
+    @downcase_text = @text.downcase
+    @downcase_array = @downcase_text.split
+    @occurrences = @downcase_array.count(@downcased_input)
 
     # ================================================================================
     # Your code goes above.
@@ -32,13 +34,12 @@ class CalculationsController < ApplicationController
     @principal = params[:principal_value].to_f
 
     # ================================================================================
-    # Your code goes below.
-    # The annual percentage rate the user input is in the decimal @apr.
-    # The number of years the user input is in the integer @years.
-    # The principal value the user input is in the decimal @principal.
+    @rate_percent = @apr/100
+    @r = @rate_percent/12
+    @exponent = @years * -12
     # ================================================================================
 
-    @monthly_payment = "Replace this string with your answer."
+    @monthly_payment = (@principal * @r) / (1 - (1 + @r)**@exponent)
 
     # ================================================================================
     # Your code goes above.
@@ -52,20 +53,15 @@ class CalculationsController < ApplicationController
     @ending = Chronic.parse(params[:ending_time])
 
     # ================================================================================
-    # Your code goes below.
-    # The start time is in the Time @starting.
-    # The end time is in the Time @ending.
-    # Note: Ruby stores Times in terms of seconds since Jan 1, 1970.
-    #   So if you subtract one time from another, you will get an integer
-    #   number of seconds as a result.
+
     # ================================================================================
 
-    @seconds = "Replace this string with your answer."
-    @minutes = "Replace this string with your answer."
-    @hours = "Replace this string with your answer."
-    @days = "Replace this string with your answer."
-    @weeks = "Replace this string with your answer."
-    @years = "Replace this string with your answer."
+    @seconds = @ending - @starting
+    @minutes = @seconds/60
+    @hours = @minutes/60
+    @days = @hours/24
+    @weeks = @days/7
+    @years = @days/365
 
     # ================================================================================
     # Your code goes above.
@@ -78,29 +74,48 @@ class CalculationsController < ApplicationController
     @numbers = params[:list_of_numbers].gsub(',', '').split.map(&:to_f)
 
     # ================================================================================
-    # Your code goes below.
-    # The numbers the user input are in the array @numbers.
+    @number_count = @numbers.count
+    @middle_value = @number_count / 2
+
     # ================================================================================
 
-    @sorted_numbers = "Replace this string with your answer."
+    @sorted_numbers = @numbers.sort
 
-    @count = "Replace this string with your answer."
+    @count = @number_count
 
-    @minimum = "Replace this string with your answer."
+    @minimum = @numbers.min
 
-    @maximum = "Replace this string with your answer."
+    @maximum = @numbers.max
 
-    @range = "Replace this string with your answer."
+    @range = @maximum - @minimum
 
-    @median = "Replace this string with your answer."
+    if @count.odd?
+      @median = @sorted_numbers[@middle_value]
+    end
 
-    @sum = "Replace this string with your answer."
+    if @count.even?
+      @value_1 = @sorted_numbers[@middle_value -1]
+      @value_2 = @sorted_numbers[@middle_value]
+      @avg = (@value_2 - @value_1) /2
+      @median = @value_1 + @avg
+    end
 
-    @mean = "Replace this string with your answer."
+    @sum = @numbers.sum
 
-    @variance = "Replace this string with your answer."
+    @mean = @sum / @count
 
-    @standard_deviation = "Replace this string with your answer."
+    @variance_calc = []
+
+    @numbers.each do |num|
+      @variance_calc.push((num - @mean)**2)
+    end
+
+    @variance_calc_total = @variance_calc.sum
+
+    @variance = (@variance_calc_total / (@count))
+
+    @standard_deviation = Math.sqrt(@variance)
+
 
     @mode = "Replace this string with your answer."
 
